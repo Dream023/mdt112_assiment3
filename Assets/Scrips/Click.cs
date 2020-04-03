c1e9e33a5f93f31c6f541bc6e59e5b11bfa715d7
Dream023 <61343046+Dream023@users.noreply.github.com>
2020-04-03 18:22:23 +0700

    Initialized git repository.

---
+++ Assets/Scrips/Click.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour
+{
+    public GameObject End;
+    private void OnMouseDrag()
+    {
+        Vector3 M = Input.mousePosition;
+        M.z = transform.position.z - Camera.main.transform.position.z;
+        M = Camera.main.ScreenToWorldPoint(M);
+        transform.position = new Vector3(M.x, M.y, transform.position.z);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.Equals(End))
+        {
+            Debug.Log(End);
+        }
+    }
+}
---
+++ Assets/Scrips/Enter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enter : MonoBehaviour
+{
+    public GameObject Box,Red;
+    public void Start()
+    {
+        Red.SetActive(true);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.Equals(Box))
+        Red.SetActive(false);
+    }
+}
